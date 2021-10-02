f007d17e90121a85237061ff6d6d68195ccae44e
sean1516 <43746619+sean1516@users.noreply.github.com>
2021-10-02 11:10:11 +0100

    Update server.py
    aaaaa

--- server.py
+++ server.py
@@ -8,9 +8,6 @@ style = ("style.scss", "style.css")
 script = ("typing-test.js", "typing-test-compiled.js")
 
 server.watch(style[0], shell(
-    ["node_modules/.bin/sass", style[0]], output=style[1]))
-server.watch(script[0], shell(
-    ["node_modules/.bin/babel", script[0]], output=script[1]))
-server.watch("index.html")
+    ["node_modules/.bin/sass", style[0]], output=style[1]server.watch("index.html")
 
 server.serve(port=8080, host="localhost", open_url_delay=True)
